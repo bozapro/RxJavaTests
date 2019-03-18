2dad203258663a4b25168b50040e8fc38270b6a8
Aleksandar Bozic <bozapro@gmail.com>
2019-03-18 23:39:03 +0100

    Added test justLongBackgroundOperationOperationRight.

--- app/src/test/java/com/example/bajob/rxjavatests/ExampleUnitTest2.kt
+++ app/src/test/java/com/example/bajob/rxjavatests/ExampleUnitTest2.kt
@@ -3,9 +3,11 @@ package com.example.bajob.rxjavatests
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
 import io.reactivex.disposables.Disposable
+import io.reactivex.internal.schedulers.IoScheduler
 import io.reactivex.observers.DisposableObserver
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import java.util.concurrent.TimeUnit
 
 class ExampleUnitTest2 {
 
@@ -26,6 +28,18 @@ class ExampleUnitTest2 {
                     .doOnError { log("onError $it") }
                     .doOnComplete { log("onComplete") }
         }
+    }
+
+    private fun someLongOperationThatReturnInteger(): Int {
+        try {
+            log("long runnig operation started")
+            Thread.sleep(3000)
+            log("long runnig operation finished")
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        return 123
     }
 
     @Test
@@ -61,6 +75,21 @@ class ExampleUnitTest2 {
                 .assertNoErrors()
                 .assertValueCount(1)
                 .assertValue("some string")
+
+        cleanup(testCall)
+        assertTrue(testCall.isDisposed)
+    }
+
+    @Test
+    fun justLongBackgroundOperationOperationRight(){
+
+        val just = Observable.just(someLongOperationThatReturnInteger())
+                .compose(loggingTransformer())
+                .subscribeOn(IoScheduler())
+
+        val testCall = just.test()
+        testCall.await(5, TimeUnit.SECONDS)
+
 
         cleanup(testCall)
         assertTrue(testCall.isDisposed)
